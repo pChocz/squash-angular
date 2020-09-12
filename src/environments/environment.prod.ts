11a60d99bf2b3dae00bc5e3ac910739f01f24a5a
Piotr Choczynski <pchoczynski@advaoptical.com>
2020-09-12 16:24:25 +0200

    applying SSL for backend communication

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   frontendUrl: "http://squash-app.win/",
-  apiUrl: "http://squash-app.win:8443/",
+  apiUrl: "https://squash-app.win:8443/",
   cookieDomain: 'squash-app.win'
 };
