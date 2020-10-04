f07d8100320009f2e90cf1b60eb9708467f7ed51
Piotr Choczynski <pchoczynski@advaoptical.com>
2020-10-04 20:34:54 +0200

    improved create new round view a little bit

--- src/app/new-round-view/new-round-view.component.ts
+++ src/app/new-round-view/new-round-view.component.ts
@@ -8,150 +8,171 @@ import { formatDate } from '@angular/common';
 import { Season } from '../season-view/model/season.model';
 import { Title } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
+import { XpPointsPerRound } from '../shared/xp-points-per-round.model';
 
 @Component({
-  selector: 'app-new-round-view',
-  templateUrl: './new-round-view.component.html',
-  styleUrls: ['./new-round-view.component.css'],
+    selector: 'app-new-round-view',
+    templateUrl: './new-round-view.component.html',
+    styleUrls: ['./new-round-view.component.css'],
 })
 export class NewRoundViewComponent implements OnInit {
-  seasonUuid: string;
-  roundNumber: number;
-  seasonNumber: number;
-  leagueName: string;
-  season: Season;
+    seasonUuid: string;
+    roundNumber: number;
+    seasonNumber: number;
+    leagueName: string;
+    season: Season;
 
-  players: Player[];
-  selectedPlayers: Player[];
-  numberOfGroups = 2;
-  availableNumberOfGroups: number[] = [1, 2, 3, 4];
-  selectedPlayersGroup: Map<number, Player[]> = new Map();
-  roundDate: Date = new Date();
+    players: Player[];
+    selectedPlayers: Player[];
+    numberOfGroups = 2;
+    availableNumberOfGroups: number[] = [1, 2, 3, 4];
+    selectedPlayersGroup: Map<number, Player[]> = new Map();
+    roundDate: Date = new Date();
 
-  constructor(
-    private route: ActivatedRoute,
-    private http: HttpClient,
-    private router: Router,
-    private titleService: Title
-  ) {
-    this.titleService.setTitle('New round');
+    xpPointsPerRound: XpPointsPerRound[];
+    availableSplits: string[] = [];
+    currentSplit: string;
 
-    this.route.queryParams.subscribe((params) => {
-      this.seasonUuid = params.seasonUuid;
-      this.roundNumber = params.roundNumber;
-    });
+    constructor(
+        private route: ActivatedRoute,
+        private http: HttpClient,
+        private router: Router,
+        private titleService: Title
+    ) {
+        this.titleService.setTitle('New round');
 
-    console.log('season id: ' + this.seasonUuid);
-    console.log('round number: ' + this.roundNumber);
+        this.route.queryParams.subscribe((params) => {
+            this.seasonUuid = params.seasonUuid;
+            this.roundNumber = params.roundNumber;
+        });
 
-    this.http
-      .get<Season>(environment.apiUrl + 'seasons/' + this.seasonUuid)
-      .pipe(map((result) => plainToClass(Season, result)))
-      .subscribe((result) => {
-        console.log(result);
-        this.season = result;
-        this.seasonNumber = this.season.seasonNumber;
-        this.leagueName = this.season.leagueName;
-      });
+        console.log('season uuid: ' + this.seasonUuid);
+        console.log('round number: ' + this.roundNumber);
 
-    this.selectedPlayersGroup.set(1, []);
-    this.selectedPlayersGroup.set(2, []);
-    this.selectedPlayersGroup.set(3, []);
-    this.selectedPlayersGroup.set(4, []);
+        this.http
+            .get<XpPointsPerRound[]>(environment.apiUrl + 'xpPoints/all-for-table')
+            .pipe(map((result) => plainToClass(XpPointsPerRound, result)))
+            .subscribe((result) => {
+                this.xpPointsPerRound = result;
+                this.xpPointsPerRound.forEach((xpPoints) => {
+                    this.availableSplits.push(xpPoints.split);
+                });
+                console.log(this.availableSplits);
+            });
 
-    this.http
-      .get<Player[]>(
-        environment.apiUrl +
-        'scoreboards/seasons/' +
-        this.seasonUuid +
-        '/players-sorted'
-      )
-      .pipe(map((result) => plainToClass(Player, result)))
-      .subscribe((result) => {
-        console.log(result);
-        this.players = result;
-      });
-  }
+        this.http
+            .get<Season>(environment.apiUrl + 'seasons/' + this.seasonUuid)
+            .pipe(map((result) => plainToClass(Season, result)))
+            .subscribe((result) => {
+                console.log(result);
+                this.season = result;
+                this.seasonNumber = this.season.seasonNumber;
+                this.leagueName = this.season.leagueName;
+            });
 
-  ngOnInit(): void { }
+        this.selectedPlayersGroup.set(1, []);
+        this.selectedPlayersGroup.set(2, []);
+        this.selectedPlayersGroup.set(3, []);
+        this.selectedPlayersGroup.set(4, []);
 
-  onNumberOfGroupsChange(value: number): void {
-    for (let groupNumber = value + 1; groupNumber <= 4; groupNumber++) {
-      this.selectedPlayersGroup.set(groupNumber, []);
+        this.http
+            .get<Player[]>(environment.apiUrl + 'scoreboards/seasons/' + this.seasonUuid + '/players-sorted')
+            .pipe(map((result) => plainToClass(Player, result)))
+            .subscribe((result) => {
+                console.log(result);
+                this.players = result;
+            });
     }
-  }
 
-  onCheckboxChange(
-    player: Player,
-    groupNumber: number,
-    selected: boolean
-  ): void {
-    if (selected) {
-      this.selectedPlayersGroup.get(groupNumber).push(player);
-    } else {
-      this.selectedPlayersGroup.set(
-        groupNumber,
-        this.selectedPlayersGroup
-          .get(groupNumber)
-          .filter((item) => item !== player)
-      );
-    }
-  }
+    ngOnInit(): void {}
 
-  sendCreateRoundRequest(): void {
-    const dateFormatted: string = formatDate(
-      this.roundDate,
-      'yyyy-MM-dd',
-      'en-US'
-    );
-
-    console.log('round number: ' + this.roundNumber);
-    console.log('round date: ' + dateFormatted);
-    console.log('season ID: ' + this.seasonUuid);
-
-    // let playerIdsGroupOne: string = Array.prototype.map.call(this.selectedPlayersGroup.get(1), (player: Player) => player.id);
-    // let playerIdsGroupTwo: string = Array.prototype.map.call(this.selectedPlayersGroup.get(2), (player: Player) => player.id);
-    // let playerIdsGroupThree: string = Array.prototype.map.call(this.selectedPlayersGroup.get(3), (player: Player) => player.id);
-    // let playerIdsGroupFour: string = Array.prototype.map.call(this.selectedPlayersGroup.get(4), (player: Player) => player.id);
-
-    // console.log("1st group: " + playerIdsGroupOne);
-    // console.log("2nd group: " + playerIdsGroupTwo);
-    // console.log("3rd group: " + playerIdsGroupThree);
-    // console.log("4th group: " + playerIdsGroupFour);
-
-    let params = new HttpParams()
-      .set('roundNumber', this.roundNumber.toString())
-      .set('roundDate', dateFormatted)
-      .set('seasonUuid', this.seasonUuid);
-
-    for (let i = 1; i <= this.numberOfGroups; i++) {
-      const currentGroupSelectedPlayers: Player[] = this.selectedPlayersGroup.get(
-        i
-      );
-      let currentGroupPlayerIds = '';
-      for (const player of this.players) {
-        if (currentGroupSelectedPlayers.includes(player)) {
-          currentGroupPlayerIds += player.id + ',';
+    onNumberOfGroupsChange(value: number): void {
+        for (let groupNumber = value + 1; groupNumber <= 4; groupNumber++) {
+            this.selectedPlayersGroup.set(groupNumber, []);
         }
-      }
-      currentGroupPlayerIds = currentGroupPlayerIds.substring(
-        0,
-        currentGroupPlayerIds.length - 1
-      );
-      console.log('current: ' + currentGroupPlayerIds);
-
-      console.log(i + ' group: ' + currentGroupPlayerIds);
-      params = params.append('playersIds', currentGroupPlayerIds);
     }
 
-    console.log(params);
+    onCheckboxChange(player: Player, groupNumber: number, selected: boolean): void {
+        if (selected) {
+            this.selectedPlayersGroup.get(groupNumber).push(player);
+        } else {
+            this.selectedPlayersGroup.set(
+                groupNumber,
+                this.selectedPlayersGroup.get(groupNumber).filter((item) => item !== player)
+            );
+        }
+        console.log(this.selectedPlayersGroup);
+        this.buildSplitBasedOnSelections();
+    }
 
-    this.http
-      .post<string>(environment.apiUrl + 'rounds', params)
-      .subscribe((result) => {
-        const newRoundUuid: string = result;
-        console.log('UUID of just created round: ' + result);
-        this.router.navigate(['round', newRoundUuid]);
-      });
-  }
+    buildSplitBasedOnSelections(): void {
+        const split = [];
+        for (const [key, value] of this.selectedPlayersGroup) {
+            if (value.length === 0) {
+                break;
+            } else {
+                split.push(value.length);
+            }
+        }
+        console.log(split.join(' | '));
+        this.currentSplit = split.join(' | ');
+    }
+
+    isSelectionValid(): boolean {
+        const isValidSplit = this.availableSplits.some((split) => split === this.currentSplit);
+        const noOverlappingPlayers = this.checkNoOverlappingPlayers();
+        return isValidSplit && noOverlappingPlayers;
+    }
+
+    checkNoOverlappingPlayers(): boolean {
+        let selectedPlayers = [];
+
+        for (const [key, playersArray] of this.selectedPlayersGroup) {
+            for (const player of playersArray) {
+                selectedPlayers.push(player);
+            }
+        }
+
+        console.log(selectedPlayers);
+        console.log(selectedPlayers.length);
+        console.log(new Set(selectedPlayers).size);
+
+        return new Set(selectedPlayers).size === selectedPlayers.length;
+    }
+
+    sendCreateRoundRequest(): void {
+        const dateFormatted: string = formatDate(this.roundDate, 'yyyy-MM-dd', 'en-US');
+
+        console.log('round number: ' + this.roundNumber);
+        console.log('round date: ' + dateFormatted);
+        console.log('season ID: ' + this.seasonUuid);
+
+        let params = new HttpParams()
+            .set('roundNumber', this.roundNumber.toString())
+            .set('roundDate', dateFormatted)
+            .set('seasonUuid', this.seasonUuid);
+
+        for (let i = 1; i <= this.numberOfGroups; i++) {
+            const currentGroupSelectedPlayers: Player[] = this.selectedPlayersGroup.get(i);
+            let currentGroupPlayerIds = '';
+            for (const player of this.players) {
+                if (currentGroupSelectedPlayers.includes(player)) {
+                    currentGroupPlayerIds += player.id + ',';
+                }
+            }
+            currentGroupPlayerIds = currentGroupPlayerIds.substring(0, currentGroupPlayerIds.length - 1);
+            console.log('current: ' + currentGroupPlayerIds);
+
+            console.log(i + ' group: ' + currentGroupPlayerIds);
+            params = params.append('playersIds', currentGroupPlayerIds);
+        }
+
+        console.log(params);
+
+        this.http.post<string>(environment.apiUrl + 'rounds', params).subscribe((result) => {
+            const newRoundUuid: string = result;
+            console.log('UUID of just created round: ' + result);
+            this.router.navigate(['round', newRoundUuid]);
+        });
+    }
 }
