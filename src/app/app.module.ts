5f8f7b0e5393c2c518fa2bcd12a17f5e3e081f47
pChocz <piotr@choczynski.pl>
2020-11-17 21:37:20 +0100

    introduced bootstrap 5, several minor changes and begin of unifying of views layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -101,7 +101,7 @@ import {CssSpinnerComponent} from './css-spinner/css-spinner.component';
 import {LeaguesSummaryComponent} from './dashboard-view/leagues-summary/leagues-summary.component';
 import {ScoreBalanceComponent} from './dashboard-view/score-balance/score-balance.component';
 import {TrophyIconComponent} from './dashboard-view/trophy-icon/trophy-icon.component';
-import { CountUpModule } from 'ngx-countup';
+import {CountUpModule} from 'ngx-countup';
 
 const cookieConfig: NgcCookieConsentConfig = {
     cookie: {
