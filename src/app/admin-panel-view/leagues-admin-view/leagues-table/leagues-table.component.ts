f4e85204a67c752c2f32b4bd40a72e40c29119de
pChocz <piotr@choczynski.pl>
2021-07-23 12:22:58 +0200

    changing form fields to 'outline' for consistency, few UI appearance fixes, support for league removal

--- src/app/admin-panel-view/leagues-admin-view/leagues-table/leagues-table.component.ts
+++ src/app/admin-panel-view/leagues-admin-view/leagues-table/leagues-table.component.ts
@@ -2,41 +2,99 @@ import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {MatSort} from "@angular/material/sort";
 import {MatTableDataSource} from "@angular/material/table";
 import {League} from "../../../shared/rest-api-dto/league.model";
+import {HttpClient} from "@angular/common/http";
+import {ApiEndpointsService} from "../../../shared/api-endpoints.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatDialog} from "@angular/material/dialog";
+import {ConfirmationDialogComponent} from "../../../confirmation-dialog/confirmation-dialog.component";
+import {BlockUI, NgBlockUI} from 'ng-block-ui';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
-    selector: 'app-leagues-table',
-    templateUrl: './leagues-table.component.html',
-    styleUrls: ['./leagues-table.component.css']
+  selector: 'app-leagues-table',
+  templateUrl: './leagues-table.component.html',
+  styleUrls: ['./leagues-table.component.css']
 })
 export class LeaguesTableComponent implements OnInit {
 
-    @Input() leagues: League[];
-    @Input() logosMap: Map<string, string>;
+  @BlockUI() blockUI: NgBlockUI;
 
-    @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @Input() leagues: League[];
+  @Input() logosMap: Map<string, string>;
 
-    dataSource: MatTableDataSource<League>;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-    displayedColumns: string[] = [
-        'number-column',
-        'league-name-column',
-        'seasons-column',
-        'league-logo-column',
-        'uuid-column',
-        'edit-button-column',
-    ];
+  dataSource: MatTableDataSource<League>;
 
-    constructor() {
-    }
+  displayedColumns: string[] = [
+    'number-column',
+    'league-name-column',
+    'seasons-column',
+    'league-logo-column',
+    'uuid-column',
+    'edit-button-column',
+    'delete-button-column',
+  ];
 
-    ngOnInit(): void {
-        this.dataSource = new MatTableDataSource(this.leagues);
-        this.dataSource.sort = this.sort;
-    }
+  constructor(private http: HttpClient,
+              private apiEndpointsService: ApiEndpointsService,
+              private snackBar: MatSnackBar,
+              private dialog: MatDialog,
+              private translateService: TranslateService) {
+  }
 
-    applyFilter(event: Event) {
-        const filterValue = (event.target as HTMLInputElement).value;
-        this.dataSource.filter = filterValue.trim().toLowerCase();
-    }
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.leagues);
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  deleteLeague(league: League) {
+
+    const confirmationDialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: {message: 'adminPanel.league.delete.areYouSure'}
+    });
+
+    confirmationDialogRef.afterClosed()
+    .subscribe(
+        result => {
+          if (result === true) {
+
+            this.translateService
+            .get('adminPanel.league.delete.deleting')
+            .subscribe((translation: string) => {
+              this.blockUI.start(translation);
+            });
+
+            this.http
+            .delete<string>(this.apiEndpointsService.getLeagueWithUuid(league.leagueUuid))
+            .subscribe(
+                () => {
+                  const index = this.dataSource.data.indexOf(league);
+                  this.dataSource.data.splice(index, 1);
+                  this.dataSource._updateChangeSubscription();
+
+                  this.snackBar.open("League " + league.leagueName + " deleted!", 'X', {
+                    duration: 7 * 1000,
+                    panelClass: ['mat-toolbar', 'mat-primary'],
+                  });
+                  this.blockUI.stop();
+
+                }, error => {
+                  this.snackBar.open("ERROR", 'X', {
+                    duration: 7 * 1000,
+                    panelClass: ['mat-toolbar', 'mat-warn'],
+                  });
+                  this.blockUI.stop();
+                });
+          }
+        });
+
+
+  }
 
 }
