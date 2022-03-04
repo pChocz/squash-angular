bbf6401e7cfb95090d6df32b2d7ff96fccb44022
Piotr Choczyński <piotr@choczynski.pl>
2022-03-04 17:37:38 +0100

    court calculator fixes

--- src/app/court-calculator-view/court-calculator-view.component.ts
+++ src/app/court-calculator-view/court-calculator-view.component.ts
@@ -15,21 +15,15 @@ export class CourtCalculatorViewComponent implements OnInit {
   MAX_PLAYERS_PER_GROUP: number = 9;
   MAX_COURTS_PER_HOUR: number = 4;
   MAX_RATE_PER_HOUR: number = 95;
-  REDUCE_PER_MULTISPORT_MINUS: number = 15;
-  REDUCE_PER_MULTISPORT_PLUS: number = 30;
-  REDUCE_PER_MULTISPORT_DOUBLE_PLUS: number = 45;
-  REDUCE_PER_MULTISPORT_MINUS_PERSON: number = 10;
-  REDUCE_PER_MULTISPORT_PLUS_PERSON: number = 15;
-  REDUCE_PER_MULTISPORT_DOUBLE_PLUS_PERSON: number = 20;
+  REDUCE_PER_MULTISPORT: number[] = [15, 30, 45, 60];
+  REDUCE_PER_MULTISPORT_PERSON: number[] = [10, 15, 20, 30];
 
   // predefined changable values
   ratePerHour: number = 60;
   playersPerGroup: number[] = [5, 5, 0, 0];
   courtsPerHour: number[] = [4, 4, 0];
   addToRound: number = 10;
-  playersMultisportMinus: number = 0;
-  playersMultisportPlus: number = 0;
-  playersMultisportDoublePlus: number = 0;
+  playersMultisport: number[] = [0, 0, 0, 0];
 
   // results
   allPlayers: number;
@@ -39,11 +33,7 @@ export class CourtCalculatorViewComponent implements OnInit {
   totalMultisportReduce: number;
   totalToPayCash: number;
   costPerPlayerFixed: number;
-  costPerPlayerNoMulti: number;
-  costPerPlayerMultiMinus: number;
-  costPerPlayerMultiPlus: number;
-  costPerPlayerMultiDoublePlus: number;
-
+  costPerPlayerReal: number[] = [0, 0, 0, 0, 0];
 
   constructor(private titleService: Title,
               private loggerService: MyLoggerService,
@@ -116,63 +106,30 @@ export class CourtCalculatorViewComponent implements OnInit {
 
 
   incrementAddToRound(): void {
-    if (this.addToRound < this.MAX_RATE_PER_HOUR) {
-      this.addToRound += 1;
-    }
+    this.addToRound += 1;
     this.update();
   }
 
   decrementAddToRound(): void {
-    if (this.addToRound > this.MIN_VALUE) {
-      this.addToRound -= 1;
+    this.addToRound -= 1;
+    this.update();
+  }
+
+  incrementPlayersMultisport(val): void {
+    const allPlayers = this.countAllPlayers();
+    const allMultisportPlayers = this.countMultisportPlayers();
+    if (allMultisportPlayers < allPlayers) {
+      this.playersMultisport[val] += 1;
     }
     this.update();
   }
 
-
-  incrementPlayersMultisportMinus(): void {
-    if (this.playersMultisportMinus < this.countAllPlayers() - this.playersMultisportPlus - this.playersMultisportDoublePlus) {
-      this.playersMultisportMinus += 1;
+  decrementPlayersMultisport(val): void {
+    if (this.playersMultisport[val] > this.MIN_VALUE) {
+      this.playersMultisport[val] -= 1;
     }
     this.update();
   }
-
-  decrementPlayersMultisportMinus(): void {
-    if (this.playersMultisportMinus > this.MIN_VALUE) {
-      this.playersMultisportMinus -= 1;
-    }
-    this.update();
-  }
-
-
-  incrementPlayersMultisportPlus(): void {
-    if (this.playersMultisportPlus < this.countAllPlayers() - this.playersMultisportMinus - this.playersMultisportDoublePlus) {
-      this.playersMultisportPlus += 1;
-    }
-    this.update();
-  }
-
-  decrementPlayersMultisportPlus(): void {
-    if (this.playersMultisportPlus > this.MIN_VALUE) {
-      this.playersMultisportPlus -= 1;
-    }
-    this.update();
-  }
-
-  incrementPlayersMultisportDoublePlus(): void {
-    if (this.playersMultisportDoublePlus < this.countAllPlayers() - this.playersMultisportMinus - this.playersMultisportPlus) {
-      this.playersMultisportDoublePlus += 1;
-    }
-    this.update();
-  }
-
-  decrementPlayersMultisportDoublePlus(): void {
-    if (this.playersMultisportDoublePlus > this.MIN_VALUE) {
-      this.playersMultisportDoublePlus -= 1;
-    }
-    this.update();
-  }
-
 
   countAllPlayers(): number {
     let allPlayers = 0;
@@ -180,6 +137,12 @@ export class CourtCalculatorViewComponent implements OnInit {
       allPlayers += count;
     }
     return allPlayers;
+  }
+
+  countMultisportPlayers() {
+    return this
+        .playersMultisport
+        .reduce((sum, current) => sum + current, 0);
   }
 
   private update() {
@@ -190,10 +153,11 @@ export class CourtCalculatorViewComponent implements OnInit {
     this.totalMultisportReduce = this.calculateMultisportReduce();
     this.totalToPayCash = this.calculateTotalToPayCash();
     this.costPerPlayerFixed = this.calculateFixedCostPerPlayer() + this.addToRound / this.countAllPlayers();
-    this.costPerPlayerNoMulti = this.calculateCostPerPlayerNoMulti();
-    this.costPerPlayerMultiMinus = this.costPerPlayerNoMulti - this.REDUCE_PER_MULTISPORT_MINUS_PERSON;
-    this.costPerPlayerMultiPlus = this.costPerPlayerNoMulti - this.REDUCE_PER_MULTISPORT_PLUS_PERSON;
-    this.costPerPlayerMultiDoublePlus = this.costPerPlayerNoMulti - this.REDUCE_PER_MULTISPORT_DOUBLE_PLUS_PERSON;
+    this.costPerPlayerReal[0] = this.calculateCostPerPlayerNoMulti();
+    this.costPerPlayerReal[1] = this.costPerPlayerReal[0] - this.REDUCE_PER_MULTISPORT_PERSON[0];
+    this.costPerPlayerReal[2] = this.costPerPlayerReal[0] - this.REDUCE_PER_MULTISPORT_PERSON[1];
+    this.costPerPlayerReal[3] = this.costPerPlayerReal[0] - this.REDUCE_PER_MULTISPORT_PERSON[2];
+    this.costPerPlayerReal[4] = this.costPerPlayerReal[0] - this.REDUCE_PER_MULTISPORT_PERSON[3];
   }
 
   private calculateMinutesPerMatch(): number {
@@ -218,9 +182,10 @@ export class CourtCalculatorViewComponent implements OnInit {
   }
 
   private calculateMultisportReduce(): number {
-    return this.playersMultisportMinus * this.REDUCE_PER_MULTISPORT_MINUS
-        + this.playersMultisportPlus * this.REDUCE_PER_MULTISPORT_PLUS
-        + this.playersMultisportDoublePlus * this.REDUCE_PER_MULTISPORT_DOUBLE_PLUS;
+    return this.playersMultisport[0] * this.REDUCE_PER_MULTISPORT[0]
+        + this.playersMultisport[1] * this.REDUCE_PER_MULTISPORT[1]
+        + this.playersMultisport[2] * this.REDUCE_PER_MULTISPORT[2]
+        + this.playersMultisport[3] * this.REDUCE_PER_MULTISPORT[3];
   }
 
   private calculateTotalToPayCash(): number {
@@ -228,10 +193,12 @@ export class CourtCalculatorViewComponent implements OnInit {
   }
 
   private calculateCostPerPlayerNoMulti(): number {
-    const reduceMultiMinus: number = this.playersMultisportMinus * this.REDUCE_PER_MULTISPORT_MINUS_PERSON;
-    const reduceMultiPlus: number = this.playersMultisportPlus * this.REDUCE_PER_MULTISPORT_PLUS_PERSON;
-    const reduceMultiDoublePlus: number = this.playersMultisportDoublePlus * this.REDUCE_PER_MULTISPORT_DOUBLE_PLUS_PERSON;
-    return (this.totalToPayCash + this.addToRound + reduceMultiMinus + reduceMultiPlus + reduceMultiDoublePlus) / (this.countAllPlayers());
+    const reduceMulti1: number = this.playersMultisport[0] * this.REDUCE_PER_MULTISPORT_PERSON[0];
+    const reduceMulti2: number = this.playersMultisport[1] * this.REDUCE_PER_MULTISPORT_PERSON[1];
+    const reduceMulti3: number = this.playersMultisport[2] * this.REDUCE_PER_MULTISPORT_PERSON[2];
+    const reduceMulti4: number = this.playersMultisport[3] * this.REDUCE_PER_MULTISPORT_PERSON[3];
+    const totalReduce: number = reduceMulti1 + reduceMulti2 + reduceMulti3 + reduceMulti4;
+    return (this.totalToPayCash + this.addToRound + totalReduce) / this.countAllPlayers();
   }
 
 }
