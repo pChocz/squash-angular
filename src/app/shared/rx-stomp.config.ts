cb5fc4c4b23e8326d02bd63fe8c1e7e4c70c7aff
Piotr Choczyński <piotr@choczynski.pl>
2022-09-26 21:44:46 +0200

    implementing websockets / further attempts

--- src/app/shared/rx-stomp.config.ts
+++ src/app/shared/rx-stomp.config.ts
@@ -6,7 +6,7 @@ export const myRxStompConfig: RxStompConfig = {
     brokerURL: environment.wsUrl,
     heartbeatIncoming: 0,           // Typical value     0 - disabled
     heartbeatOutgoing: 20000,       // Typical value 20000 - every 20 seconds
-    reconnectDelay: 1000,           // Typical value   500 - 0.5 second
+    reconnectDelay: 5000,          // Typical value   500 - 0.5 second
 
     // Will log diagnostics on console - should be disabled on production
     debug: (msg: string): void => {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     frontendUrl: 'https://squash-app.win/',
     apiUrl: 'https://api.squash-app.win/',
-    wsUrl: 'ws://squash-app.win/ws',
+    wsUrl: 'wss://squash-app.win/ws',
     cookieDomain: 'squash-app.win',
 };
