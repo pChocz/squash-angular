9cbcae580718b3ac64576c99339b3f4fdd147286
Piotr Choczyński <piotr@choczynski.pl>
2022-09-27 06:05:00 +0200

    implementing websockets / further attempts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     frontendUrl: 'https://squash-app.win/',
     apiUrl: 'https://api.squash-app.win/',
-    wsUrl: 'wss://squash-app.win/ws',
+    wsUrl: 'wss://api.squash-app.win/ws',
     cookieDomain: 'squash-app.win',
 };
