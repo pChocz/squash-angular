e8b8749ef5b32920d75a9c3d9045a4ddd5768cfb
Piotr Choczyński <piotr@choczynski.pl>
2022-09-27 17:39:37 +0200

    implementing websockets / finalizing

--- src/app/shared/rx-stomp.config.ts
+++ src/app/shared/rx-stomp.config.ts
@@ -6,19 +6,18 @@ export const myRxStompConfig: RxStompConfig = {
     brokerURL: environment.wsUrl,
     heartbeatIncoming: 0,           // Typical value     0 - disabled
     heartbeatOutgoing: 20000,       // Typical value 20000 - every 20 seconds
-    reconnectDelay: 5000,          // Typical value   500 - 0.5 second
+    reconnectDelay: 1000,           // Typical value   500 - 0.5 second
 
-    // Will log diagnostics on console - should be disabled on production
+    // Will log diagnostics on console for non-production environment
     debug: (msg: string): void => {
-        console.log(new Date(), msg);
+        !environment.production && console.log(new Date(), msg);
     },
-
-    logRawCommunication: true,
+    logRawCommunication: !environment.production,
 
     // Headers
     // Typical keys: login, passcode, host
-    connectHeaders: {
-        login: 'guest',
-        passcode: 'guest',
-    },
+    // connectHeaders: {
+    //     login: 'guest',
+    //     passcode: 'guest',
+    // },
 };
