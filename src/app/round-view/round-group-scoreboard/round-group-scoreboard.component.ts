f06bac591dea517898d05ca2152facfed73f7eb9
Piotr Choczyński <piotr@choczynski.pl>
2022-10-16 11:38:44 +0200

    h2h stats for round in edit-mode / button to navigate to players stats for a round group

--- src/app/round-view/round-group-scoreboard/round-group-scoreboard.component.ts
+++ src/app/round-view/round-group-scoreboard/round-group-scoreboard.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {RoundGroupScoreboard} from '../../shared/rest-api-dto/round-group-scoreboard.model';
+import {Router} from "@angular/router";
+import {formatDate} from "@angular/common";
 
 @Component({
     selector: 'app-round-group-scoreboard',
@@ -27,9 +29,32 @@ export class RoundGroupScoreboardComponent implements OnInit {
         'points-balance-column',
     ];
 
-    constructor() {
+    constructor(private router: Router) {
     }
 
     ngOnInit(): void {
+    }
+
+    navigateToPlayerStats(): void {
+        let leagueUuid = this.roundGroupScoreboard.matches[0].leagueUuid;
+        let playerUuids = this.roundGroupScoreboard.scoreboardRows.map(row => row.player.uuid);
+        let roundDateMinusOneDay = new Date(this.roundGroupScoreboard.matches[0].date);
+        roundDateMinusOneDay.setDate(roundDateMinusOneDay.getDate() - 1);
+        let roundDateMinusOneYear = new Date(this.roundGroupScoreboard.matches[0].date);
+        roundDateMinusOneYear.setFullYear(roundDateMinusOneDay.getFullYear() - 1);
+
+        this.router.navigate(
+            [
+                `/league-players`,
+                leagueUuid
+            ],
+            {
+                queryParams: {
+                    players: playerUuids.toString(),
+                    dateFrom: formatDate(roundDateMinusOneYear, 'yyyy-MM-dd', 'en-US'),
+                    dateTo: formatDate(roundDateMinusOneDay, 'yyyy-MM-dd', 'en-US')
+                },
+                skipLocationChange: true
+            });
     }
 }
