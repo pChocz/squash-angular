0985b2827bf90b1d283a87644519e889ff305843
Piotr Choczyński <piotr@choczynski.pl>
2023-10-03 20:38:04 +0200

    disabled / locked players handling

--- src/app/shared/rest-api-dto/player.model.ts
+++ src/app/shared/rest-api-dto/player.model.ts
@@ -3,6 +3,8 @@ export class Player {
     public uuid: string;
     public username: string;
     public emoji: string;
+    public enabled: boolean;
+    public nonLocked: boolean;
 
     public toString(): string {
         return this.username;
