ba9066d2e1cde1ab03d61deb1e63453bd4cad289
Piotr Choczyński <piotr@choczynski.pl>
2024-09-16 20:13:37 +0200

    pjoter1v

---
+++ src/environments/environment.prod-pjoter1v.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    frontendUrl: 'https://squashable.pjoter1v.pl/',
+    apiUrl: 'https://squashable-api.pjoter1v.pl/',
+    wsUrl: 'wss://squashable-api.pjoter1v.pl/ws',
+    cookieDomain: 'squashable.pjoter1v.pl',
+};
