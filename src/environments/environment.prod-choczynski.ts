bd0c7238184dda3b2e300a9cda5d25221b2e38d0
Piotr Choczyński <piotr@choczynski.pl>
2024-10-20 17:44:22 +0200

    config for choczynski.pl domain

---
+++ src/environments/environment.prod-choczynski.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    frontendUrl: 'https://squashable.choczynski.pl/',
+    apiUrl: 'https://squashable-api.choczynski.pl/',
+    wsUrl: 'wss://squashable-api.choczynski.pl/ws',
+    cookieDomain: 'squashable.choczynski.pl',
+};
